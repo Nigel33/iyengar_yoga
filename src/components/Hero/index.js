f9357cd6ebe1f8642aa759f01d0d91648c107443
Nigel Hing <nigelhing1@gmail.com>
2020-10-18 01:07:50 +0800

    first graph ql image

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -4,45 +4,159 @@ import { Container, Row, Col, Button, Carousel } from 'react-bootstrap'
 import garland from '../Images/garland.jpg'
 import family from '../Images/gurujiFamily.jpg'
 import newGeneration from '../Images/newGeneration.jpg'
+import Img from 'gatsby-image'
+import { useStaticQuery, graphql } from "gatsby"
 
-const Hero = () => {
-  return (
-    // <div className="hero-lg">
-    //   <section className="cta d-flex align-items-center h-100">
-    //     <article className="w-50 text-light ml-auto">
-    //       <blockquote class="blockquote">
-    //         <h2 class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</h2>
-    //       </blockquote>
-    //       <span>Swami G</span>
-    //     </article>
-    //   </section>
-    // </div> 
-    <div className="hero-section">
-      <Carousel style={{ width: '100%', height: '700px', margin: 'auto' }}>
-        <Carousel.Item>
-          <img                  
-            src={garland}
-            alt="First slide"
-            style={{ width: '100%', height: '700px', objectFit: 'cover' }}                  
-          />          
-        </Carousel.Item>
-        {/* <Carousel.Item>
-          <img                  
-            src={family}
-            alt="2nd slide" 
-            style={{ width: '100%', height: '700px', objectFit: 'cover' }}                 
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img                  
-            src={newGeneration}
-            alt="Third slide"
-            style={{ width: '100%', height: '700px', objectFit: 'cover' }}                   
-          />          
-        </Carousel.Item>  */}
-      </Carousel>
-    </div>
-  );   
+export default () => {
+  const img_1 = useStaticQuery(graphql`
+  query{
+    file(
+      relativePath: { eq: "garland.jpg"}      
+    ) {
+      childImageSharp {
+        fluid(maxWidth: 1200) {
+          base64
+          aspectRatio
+          src
+          srcSet
+          sizes
+        }
+      }
+    }
+  }
+    `)
+    
+    return (
+      // <div className="hero-lg">
+      //   <section className="cta d-flex align-items-center h-100">
+      //     <article className="w-50 text-light ml-auto">
+      //       <blockquote class="blockquote">
+      //         <h2 class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</h2>
+      //       </blockquote>
+      //       <span>Swami G</span>
+      //     </article>
+      //   </section>
+      // </div> 
+      <div className="hero-section">
+        <Carousel style={{ width: '100%', height: '700px', margin: 'auto' }}>
+          <Carousel.Item>
+            <Img fluid={img_1.file.childImageSharp.fluid}
+              alt="test" />
+            {/* <img                  
+              src={garland}
+              alt="First slide"
+              style={{ width: '100%', height: '700px', objectFit: 'cover' }}                  
+            />           */}
+          </Carousel.Item>         
+          {/* <Carousel.Item>
+            <img                  
+              src={family}
+              alt="2nd slide" 
+              style={{ width: '100%', height: '700px', objectFit: 'cover' }}                 
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img                  
+              src={newGeneration}
+              alt="Third slide"
+              style={{ width: '100%', height: '700px', objectFit: 'cover' }}                   
+            />          
+          </Carousel.Item>  */}
+        </Carousel>
+      </div>
+    );   
+
 }
 
-export default Hero
+// export const query = graphql`
+//   query{
+//     file(
+//       relativePath: { eq: "path-to-file/myfile.jpg"}
+//     ) {
+//       childImageSharp {
+//         fluid(maxWidth: 1200) {
+//           base64
+//           aspectRatio
+//           src
+//           srcSet
+//           sizes
+//         }
+//       }
+//     }
+//   }
+// `
+
+// export default () => {
+//   const data = useStaticQuery(graphql`
+//   query MyQuery {
+//   file(relativeDirectory: {eq: "garland.jpg"}) { 
+//     childImageSharp: {
+//       fluid: { 
+//         base64
+//         aspectRatio
+//         sizes
+//         src
+//         srcSet             
+//       }
+//     }
+//   }
+// }
+//   `)
+// }
+
+// query MyQuery {
+//   file(relativeDirectory: {eq: "garland.jpg"}, 
+//     childImageSharp: {
+//       fluid: { 
+//         base64
+//         aspectRatio
+//         sizes
+//         src
+//         srcSet             
+//       }
+//     }
+// }
+
+// const Hero = () => {
+//   return (
+//     // <div className="hero-lg">
+//     //   <section className="cta d-flex align-items-center h-100">
+//     //     <article className="w-50 text-light ml-auto">
+//     //       <blockquote class="blockquote">
+//     //         <h2 class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</h2>
+//     //       </blockquote>
+//     //       <span>Swami G</span>
+//     //     </article>
+//     //   </section>
+//     // </div> 
+//     <div className="hero-section">
+//       <Carousel style={{ width: '100%', height: '700px', margin: 'auto' }}>
+//         <Carousel.Item>
+//           <Img fluid={data.file.childImageSharp.fluid}
+//             alt="test" />
+//           {/* <img                  
+//             src={garland}
+//             alt="First slide"
+//             style={{ width: '100%', height: '700px', objectFit: 'cover' }}                  
+//           />           */}
+//         </Carousel.Item>
+//         {/* <Carousel.Item>
+//           <img                  
+//             src={family}
+//             alt="2nd slide" 
+//             style={{ width: '100%', height: '700px', objectFit: 'cover' }}                 
+//           />
+//         </Carousel.Item>
+//         <Carousel.Item>
+//           <img                  
+//             src={newGeneration}
+//             alt="Third slide"
+//             style={{ width: '100%', height: '700px', objectFit: 'cover' }}                   
+//           />          
+//         </Carousel.Item>  */}
+//       </Carousel>
+//     </div>
+//   );   
+// }
+
+// export default Hero
